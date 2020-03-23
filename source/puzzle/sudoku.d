3b0c45dd8f6a09762d073cb7b8686e961c81d638
João Lourenço <jlourenco5691@gmail.com>
2020-03-23 15:33:06 +0000

    sudoku: fixed ctor and functions dim var, these were replaced with size
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/puzzle/sudoku.d
+++ source/puzzle/sudoku.d
@@ -11,12 +11,12 @@ public abstract class Sudoku
 
     this(in uint[][] puzzle)
     {
-        grid = new Grid(9, 9, puzzle);
+        grid = new Grid(9, puzzle);
     }
 
-    this(in uint x, in uint y, in uint[][] puzzle)
+    this(in uint side, in uint[][] puzzle)
     {
-        grid = new Grid(x, y, puzzle);
+        grid = new Grid(side, puzzle);
     }
 
 
@@ -32,20 +32,20 @@ public abstract class Sudoku
         // sees whether or not the value in the current cell was a given one
         if (grid[row, column].blocked)
         {
-            if (column < grid.dim.x - 1)   return backtrack(column + 1, row);
-            else if (row < grid.dim.x - 1) return backtrack(0, row + 1);
+            if (column < grid.side - 1)   return backtrack(column + 1, row);
+            else if (row < grid.side - 1) return backtrack(0, row + 1);
             else                           return grid[row, column].n;
         }
 
         uint ret;
-        for (uint i = 1; i <= grid.dim.x; i++)
+        for (uint i = 1; i <= grid.side; i++)
         {
             if (!grid.find(row, column, i))
             {
                 grid[row, column].n = i;
 
-                if (column < grid.dim.x - 1)   ret = backtrack(column + 1, row);
-                else if (row < grid.dim.x - 1) ret = backtrack(0, row + 1);
+                if (column < grid.side - 1)   ret = backtrack(column + 1, row);
+                else if (row < grid.side - 1) ret = backtrack(0, row + 1);
                 else                           return i;
 
                 if (ret != 0) return ret;
